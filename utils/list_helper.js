358a3d008837e197e2ef2bdec50bf6325ae45ca9
cesarhenrq <cesaarhvb@gmail.com>
2023-07-29 03:11:59 -0300

    Exercise 4.6*: helper functions and unit tests, step4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -14,8 +14,44 @@ const favoriteBlog = (blogs) => {
   );
 };
 
+const mostBlogs = (blogs) => {
+  const authors = blogs.reduce((authors, blog) => {
+    const author = authors.find((author) => author.author === blog.author);
+    if (author) {
+      author.blogs += 1;
+    } else {
+      authors.push({ author: blog.author, blogs: 1 });
+    }
+    return authors;
+  }, []);
+
+  return authors.reduce(
+    (max, author) => (max && max.blogs > author.blogs ? max : author),
+    null
+  );
+};
+
+const mostLikes = (blogs) => {
+  const authors = blogs.reduce((authors, blog) => {
+    const author = authors.find((author) => author.author === blog.author);
+    if (author) {
+      author.likes += blog.likes;
+    } else {
+      authors.push({ author: blog.author, likes: blog.likes });
+    }
+    return authors;
+  }, []);
+
+  return authors.reduce(
+    (max, author) => (max && max.likes > author.likes ? max : author),
+    null
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 };
