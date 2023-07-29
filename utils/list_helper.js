a553df29af7e6eba0a4e906670cb53a5b537b4ae
cesarhenrq <cesaarhvb@gmail.com>
2023-07-29 02:54:40 -0300

    Exercise 4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce(
+    (max, blog) => (max && max.likes > blog.likes ? max : blog),
+    null
+  );
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
