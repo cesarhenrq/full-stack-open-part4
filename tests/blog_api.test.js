00d1be3af0d99261c12005adb99ffa54d43f8ac3
cesarhenrq <cesaarhvb@gmail.com>
2023-07-31 03:24:50 -0300

    Exercise 4.15: bloglist expansion, step3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const Blog = require('../models/blog');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 const helper = require('./test_helper');
 const app = require('../app');
 
@@ -170,6 +172,46 @@ describe('updating a blog', () => {
       .put(`/api/blogs/${blogToUpdate.id}`)
       .send(updatedBlog)
       .expect(400);
+  });
+});
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('sekret', 10);
+    const user = new User({ username: 'root', passwordHash });
+
+    await user.save();
+  });
+
+  test('users are returned as json', async () => {
+    await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen'
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
   });
 });
 
